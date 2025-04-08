87f337a81d9860a33c109cd4f8bf3e18367b25cc
dasu-archive <dasu.archive@gmail.com>
2025-04-08 09:32:22 +0900

    News Scrap 완료

--- 20250408_NewsScrap/01_news_scrap.py
+++ 20250408_NewsScrap/01_news_scrap.py
@@ -1,17 +1,12 @@
+from bs4 import BeautifulSoup
 import requests
-from lxml import html
 
-url = 'https://news.daum.net/economy'
-
+url = 'https://news.daum.net/economy'  
 response = requests.get(url)
-response.raise_for_status()
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'html.parser')
 
-tree = html.fromstring(response.content)
+strong_tags = soup.select("ul.list_newsheadline2 > li > a > div.cont_thumb > strong.tit_txt")
 
-xpath = '//*[@id="0e289a02-ba69-440a-8a28-2b0479c64993"]/ul/li[1]/a/div[2]/strong'
-title_element = tree.xpath(xpath)
-
-if title_element:
-    print("뉴스 제목:", title_element[0].text_content().strip())
-else:
-    print("")
+for tag in strong_tags:
+    print(tag.getText(strip=True))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 
+- 250408_NewsScrap : 뉴스 스크래핑 연습문제
 - 250414_PythonExercise : 파이썬 연습문제 
